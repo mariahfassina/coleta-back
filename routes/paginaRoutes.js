75651bcd8fab6371eab1c3e3f4e737dce03f6465
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-08-11 18:13:26 -0300

    Update paginaRoutes.js

--- routes/paginaRoutes.js
+++ routes/paginaRoutes.js
@@ -1,5 +1,3 @@
-// routes/paginaRoutes.js
-
 import express from 'express';
 import {
   createPagina,
@@ -13,20 +11,9 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// ✅ ROTA PRIVADA: Para o dashboard listar todas as páginas.
-// Ex: GET /api/paginas/admin
 router.get('/admin', protect, getPaginas);
-
-// ✅ ROTA PÚBLICA: Para o site buscar páginas pelo nome (slug).
-// É impossível de confundir com um ID por causa do '/slug/'.
-// Ex: GET /api/paginas/slug/quem-somos
 router.get('/slug/:slug', getPaginaBySlug);
-
-// ✅ ROTA PRIVADA: Para criar uma nova página.
 router.post('/', protect, createPagina);
-
-// ✅ ROTA PRIVADA: Para buscar, atualizar ou deletar uma página pelo seu número (ID).
-// Ex: GET /api/paginas/60f7e2a4b9d3b13f4c6e8a1
 router
   .route('/:id')
   .get(protect, getPaginaById)
