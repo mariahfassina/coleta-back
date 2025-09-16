d07f8d0d312ff854b28169759d316ceea1edee94
mariafernandasant0s <20233017583@estudantes.ifpr.edu.br>
2025-09-16 10:25:24 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,16 +4,17 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    // Tenta se conectar ao MongoDB usando a URI do arquivo .env
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
     
-    // Se a conexão for bem-sucedida, imprime uma mensagem no console
+
     console.log(`MongoDB Conectado: ${conn.connection.host}`);
   } catch (error) {
-    // Se a conexão falhar, imprime o erro e encerra o processo do servidor
+ 
     console.error(`Erro ao conectar ao MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
+
 
 export default connectDB;
