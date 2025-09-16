772cb8db29d98f3180812aa524868964b0c4d1ad
mariafernandasant0s <20233017583@estudantes.ifpr.edu.br>
2025-09-16 10:31:15 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,9 +6,7 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// ========================
-// Configuração do Multer
-// ========================
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'), // pasta para salvar arquivos
   filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname)) // nome único
@@ -21,26 +19,18 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage, fileFilter });
 
-// ========================
-// ROTAS PÚBLICAS
-// ========================
+
 router.post('/login', loginAdmin);
 
-// ========================
-// ROTAS PROTEGIDAS
-// ========================
+
 router.get('/admins', protect, getAdmins);
 router.get('/me', protect, getMe);
 
-// ========================
-// ROTA DE UPLOAD DE IMAGEM
-// ========================
+
 router.post('/upload', protect, upload.single('foto'), (req, res) => {
   if (!req.file) return res.status(400).send('Nenhum arquivo enviado');
   res.json({ mensagem: 'Upload OK', arquivo: req.file.filename });
 });
 
-// ========================
-// Exporta o router
-// ========================
 export default router;
+
